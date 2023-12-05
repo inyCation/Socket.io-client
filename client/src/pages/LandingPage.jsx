37c246a7c574ec3e78fa1a860ef88c2699f0a505
Deepak Raj <deepak@Deepaks-MacBook-Air.local>
2023-12-05 20:16:38 +0530

    landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,8 +4,10 @@ const LandingPage = () => {
 
   return (
     <>
+      <div className="landingPageContainer">
         <h1>Welcome to Chat Room</h1>
         <h4>Chat with SEC++</h4>
+      </div>
     </>
   )
 }
